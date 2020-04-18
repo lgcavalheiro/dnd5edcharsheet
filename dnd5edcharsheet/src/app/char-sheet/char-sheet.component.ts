da1c26a5dc42790c023c232033b0517259f3f310
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-04-18 15:01:12 +0000

    minor optimization

--- dnd5edcharsheet/src/app/char-sheet/char-sheet.component.ts
+++ dnd5edcharsheet/src/app/char-sheet/char-sheet.component.ts
@@ -17,20 +17,29 @@ export class CharSheetComponent implements OnInit {
   }
 
   attribMods = {
-      str: this.calculateMod(this.attributes.str),
-      dex: this.calculateMod(this.attributes.dex),
-      con: this.calculateMod(this.attributes.con),
-      int: this.calculateMod(this.attributes.int),
-      wis: this.calculateMod(this.attributes.wis),
-      chr: this.calculateMod(this.attributes.chr)
+      str: 0,
+      dex: 0,
+      con: 0,
+      int: 0,
+      wis: 0,
+      chr: 0
     }
 
-    calculateMod(value: number) {
-      return Math.floor((value/2) - 5);
+    calculateMod(value: number, mod: string) {
+      let calc = Math.floor((value/2) - 5);
+      this.attribMods[mod] = calc;
+      return calc;
     }
 
   constructor() {
-
+    this.attribMods = {
+      str: this.calculateMod(this.attributes.str, 'str'),
+      dex: this.calculateMod(this.attributes.dex, 'dex'),
+      con: this.calculateMod(this.attributes.con, 'con'),
+      int: this.calculateMod(this.attributes.int, 'int'),
+      wis: this.calculateMod(this.attributes.wis, 'wis'),
+      chr: this.calculateMod(this.attributes.chr, 'chr')
+    }
    }
 
   ngOnInit(): void {
