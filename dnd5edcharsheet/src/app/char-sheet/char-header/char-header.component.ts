153b9f2e6c4f3df0a1856266ca5059c40f54c591
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-04-19 15:04:55 +0000

    added styles folder and fixed some of char-header

--- dnd5edcharsheet/src/app/char-sheet/char-header/char-header.component.ts
+++ dnd5edcharsheet/src/app/char-sheet/char-header/char-header.component.ts
@@ -12,4 +12,62 @@ export class CharHeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  classes = [
+    { name: 'Barbarian' },
+    { name: 'Bard' },
+    { name: 'Cleric' },
+    { name: 'Druid' },
+    { name: 'Fighter' },
+    { name: 'Monk' },
+    { name: 'Paladin' },
+    { name: 'Ranger' },
+    { name: 'Rogue' },
+    { name: 'Sorcerer' },
+    { name: 'Warlock' },
+    { name: 'Wizard' },
+  ];
+
+  backgrounds = [
+    { name: 'Acolyte' },
+    { name: 'Charlatan' },
+    { name: 'Criminal/Spy' },
+    { name: 'Entertainer' },
+    { name: 'Folk Hero' },
+    { name: 'Gladiator' },
+    { name: 'Guild Artisan/Guild Merchant' },
+    { name: 'Hermit' },
+    { name: 'Knight' },
+    { name: 'Noble' },
+    { name: 'Outlander' },
+    { name: 'Pirate' },
+    { name: 'Sage' },
+    { name: 'Sailor' },
+    { name: 'Soldier' },
+    { name: 'Urchin' },
+  ];
+
+  races = [
+    { name: 'Dragonborn' },
+    { name: 'Dwarf' },
+    { name: 'Elf' },
+    { name: 'Gnome' },
+    { name: 'Half-Elf' },
+    { name: 'Half-Orc' },
+    { name: 'Halfling' },
+    { name: 'Human' },
+    { name: 'Tiefling' },
+  ];
+
+  alignments = [
+    { name: 'Lawful good (LG)' },
+    { name: 'Neutral good (NG)' },
+    { name: 'Chaotic good (CG)' },
+    { name: 'Lawful neutral (LN)' },
+    { name: 'True neutral (TN)' },
+    { name: 'Chaotic neutral (CN)' },
+    { name: 'Lawful evil (LE)' },
+    { name: 'Neutral evil (NE)' },
+    { name: 'Chaotic evil (CE)' },
+  ]
+
 }
